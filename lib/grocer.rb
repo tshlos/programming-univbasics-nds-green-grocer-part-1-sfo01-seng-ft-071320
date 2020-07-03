8c165a518a580518ff18507c208396388817c3f5
Taci Shlosberg <tshlosberg@gmail.com>
2020-07-03 16:09:37 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,8 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  
+    collection.find { |item| item[:item] == name }
 end
 
 def consolidate_cart(cart)
@@ -10,7 +11,13 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  
+  result = cart.group_by { |item| item[:item] }
+  result.map do |key, value|
+      item = value[0]
+      item[:count] = value.length
+      item
+  end
 end
 
 
